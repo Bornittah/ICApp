b5c5ba4bc20a64f3de08f65a7fc4f41efdf7efce
2014bcs010 <openjamosesopm@gmail.com>
2018-05-05 11:21:33 +0300

    Updates

--- app/src/main/java/com/example/bornittah/icapp/MainPageActivity.java
+++ app/src/main/java/com/example/bornittah/icapp/MainPageActivity.java
@@ -1,28 +1,41 @@
 package com.example.bornittah.icapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainPageActivity extends Activity {
     ListView listView;
-    ArrayList items = new ArrayList();
+    List items = new ArrayList();
+    private Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         listView = (ListView)findViewById(R.id.listview1);
+        setView();
     }
-        public ArrayList getItems(){
-            items.add("Savings");
-            items.add("Checking");
-            items.add("Loans");
 
-            return null;
+    private void setView(){
+        items = new ArrayList();
+        items.add("Savings");
+        items.add("Checking");
+        items.add("Loans");
+        try{
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    context,
+                    android.R.layout.simple_list_item_1,
+                    items
+            );
+            listView.setAdapter(adapter);
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
+    }
 
 }
